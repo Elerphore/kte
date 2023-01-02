b1d81b222a9b72bc2fef4a2587c9b241ff9a17f0
Eremey Malinovsky <e.malinovsky@quickresto.ru>
2023-01-02 15:05:57 +0500

    chore: initial commit

---
+++ src/main/java/ru/elerphore/kte/KteApplication.java
@@ -0,0 +1,13 @@
+package ru.elerphore.kte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KteApplication.class, args);
+	}
+
+}
